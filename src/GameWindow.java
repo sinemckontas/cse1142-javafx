ff80191412abfbae518446ae05392f7ca5664f2a
Sinem C. Kontaş <sinemkontas@gmail.com>
2021-11-17 10:50:38 +0300

    Improvement on initial GUI

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,28 +1,47 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+
+import javax.swing.*;
+
+import static javafx.scene.text.FontWeight.BOLD;
+//import javax.xml.soap.Text;
 
 
 public class GameWindow extends Application {
 
 
 
-
-
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        Text text= new Text("Welcome to the game\n \n Please select action");
+        text.setStyle("-fx-font-weight: bold");
+
+      //  Label label= new Label("this is a label");
+
         Pane canvas = new Pane();
-        VBox vbox = new VBox(30); // spacing = 8
+        VBox vbox = new VBox( 100); // spacing = 8
+        vbox.setPrefSize(800, 800);
+        vbox.setAlignment(Pos.CENTER);
         canvas.getChildren().add(vbox);
         canvas.setPadding(new Insets(100,100,100,100));
-        vbox.setBackground(new Background(new BackgroundFill(Color.CRIMSON, CornerRadii.EMPTY, Insets.EMPTY)));
-        vbox.getChildren().addAll(new Button("1"), new Button("2"), new Button("3"));
-        primaryStage.setScene(new Scene(canvas, 300,300));
+        vbox.setBackground(new Background(new BackgroundFill(Color.DARKOLIVEGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+        vbox.getChildren().addAll(text, new Button("Attack"), new Button("Defend"), new Button("Wait"));
+        vbox.setPadding(new Insets(15));
+        primaryStage.setScene(new Scene(canvas, 800,800));
         primaryStage.show();
 
     }
