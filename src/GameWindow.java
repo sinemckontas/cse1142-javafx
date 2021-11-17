a38150b1fb6b5efdd29650823d13decd83a75e00
Sinem C. Kontaş <sinemkontas@gmail.com>
2021-11-17 23:07:30 +0300

    Improvements

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -13,6 +13,10 @@ import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
 import javafx.scene.text.Font;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import javax.swing.*;
 
@@ -27,21 +31,57 @@ public class GameWindow extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        FileInputStream inputstream = new FileInputStream("img/minecraft-clipart-minecraft-heart.png");
+        Image livesfull = new Image(inputstream);
+
+
+        ImageView viewLivesFull = new ImageView(livesfull);
+        ImageView viewLivesFull2= new ImageView(livesfull);
+        ImageView viewLivesFull3= new ImageView(livesfull);
+
+
+
+        viewLivesFull.setFitHeight(30);
+        viewLivesFull.setFitWidth(30);
+        viewLivesFull2.setFitHeight(30);
+        viewLivesFull2.setFitWidth(30);
+        viewLivesFull3.setFitHeight(30);
+        viewLivesFull3.setFitWidth(30);
+
+
+        Button loadbutton= new Button("Load Previous Game");
+        loadbutton.setPadding(new Insets(0, 200, 0, 0));
+
+
+        HBox hBox1= new HBox();
+        HBox hBox= new HBox();
+
+        hBox.getChildren().addAll(viewLivesFull, viewLivesFull2, viewLivesFull3);
+        hBox.setPadding(new Insets(0, 0, 0, 200));
+
+
+        hBox1.getChildren().addAll(loadbutton, hBox);
+        hBox1.setAlignment(Pos.TOP_CENTER);
+
         Text text= new Text("Welcome to the game\n \n Please select action");
         text.setStyle("-fx-font-weight: bold");
 
+
+
+
       //  Label label= new Label("this is a label");
+
 
         Pane canvas = new Pane();
         VBox vbox = new VBox( 100); // spacing = 8
-        vbox.setPrefSize(800, 800);
-        vbox.setAlignment(Pos.CENTER);
+        vbox.setPrefSize(700, 700);
+        vbox.setAlignment(Pos.TOP_CENTER);
         canvas.getChildren().add(vbox);
         canvas.setPadding(new Insets(100,100,100,100));
         vbox.setBackground(new Background(new BackgroundFill(Color.DARKOLIVEGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
-        vbox.getChildren().addAll(text, new Button("Attack"), new Button("Defend"), new Button("Wait"));
+        vbox.getChildren().addAll(hBox1, text, new Button("Attack"), new Button("Defend"), new Button("Wait"));
         vbox.setPadding(new Insets(15));
-        primaryStage.setScene(new Scene(canvas, 800,800));
+        primaryStage.setScene(new Scene(canvas, 700,700));
         primaryStage.show();
 
     }
